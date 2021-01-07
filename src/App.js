eb021a56014b3a74c375fb8bdd1e73c9d53f812e
Yonghyun <dydwk231@gmail.com>
2021-01-08 07:55:16 +0900

    set init-state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import React from "react";
+import React, { Component } from "react";
 import Todotemp from "./components/Todotemp";
 import Form from "./components/Form";
 import TodoItemList from "./components/TodoItemList";
 
-const App = () => {
-  return (
-    <Todotemp form={<Form />}>
-      <TodoItemList />
-    </Todotemp>
-  );
-};
+class App extends Component {
+  id = 3;
+
+  state = {
+    input: "",
+    todos: [
+      { id: 0, text: "리액트", checked: false },
+      { id: 1, text: "리액트", checked: true },
+      { id: 2, text: "리액트", checked: false },
+    ],
+  };
+  render() {
+    return (
+      <Todotemp form={<Form />}>
+        <TodoItemList />
+      </Todotemp>
+    );
+  }
+}
 
 export default App;
